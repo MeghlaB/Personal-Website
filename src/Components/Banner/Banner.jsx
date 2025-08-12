7ed9cff6fadb15e835fcfe56258af679d1ce182f
MeghlaB <meghlab964@gmail.com>
2025-08-12 21:23:46 +0600

    added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,33 +5,8 @@ import { IoLogoLinkedin } from "react-icons/io";
 function Banner() {
   return (
     <section className="home" id="home">
-    {/* Text-side */}
-      <div className="home-content">
-        <h3>Hi , Myself</h3>
-        <h1>Meghla Biswas</h1>
-        <h1>And , I'm a Frontend Developer</h1>
-        <p>
-          Hi, I'm Meghla Biswas, a Frontend Developer. I design responsive,
-          visually appealing, and user-friendly websites to bring your ideas to
-          life.
-        </p>
-        {/* Social-Media */}
-        <div className="social-media">
-            <a href="#"><FaFacebook/></a>
-            <a href="#"><FaTwitter /></a>
-            <a href="#"><FaGithub /></a>
-            <a href="#"><IoLogoLinkedin /></a>
-        </div>
-        {/* Download-cv */}
-        <a href="#">Download cv</a>
-      </div>
-      {/* image-side */}
-      <div className="home-img">
-      <img src="/src/assets/web_meghla.png">
-
-      </img>
-
-      </div>
+  
+   
     </section>
   );
 }
