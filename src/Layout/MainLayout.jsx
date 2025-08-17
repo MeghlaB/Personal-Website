d373558aaa305c3440d2f86084dd7afdfa12f904
MeghlaB <meghlab964@gmail.com>
2025-08-17 23:55:49 +0600

    edit section

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Navbar from '../Components/Header/Navbar'
+
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer/Footer'
+import MenuBar from '../Components/Header/Navbar'
 // className='mx-2 md:mx-4 lg:mx-6 bg-[#F8F5F4]'
 function MainLayout() {
   return (
-    <div className='bg-[#0D0518] min-h-screen'>
+    <div className='min-h-screen' style={{
+      background: "radial-gradient(125% 125% at 50% 100%, #000000 40%, #010133 100%)"
+    }}>
      <header >
-        <Navbar/>
+        <MenuBar/>
       </header>
       <Outlet></Outlet>
       <Footer></Footer>
